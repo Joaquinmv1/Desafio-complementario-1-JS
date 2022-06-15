b8a6644a53ba7a344f06396e694db8176c37e33a
Joaquinvillarreal <villarrealjoaquin09@gmail.com>
2022-06-15 14:05:52 -0300

    forwhile

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 // FOR
-// let cantPelicula = parseInt(prompt("Cuantas peliculas queres elegir?"))
-// let nombrePelicula = "";
-// let precioPelicula = 0;
-// let sumaTotal= 0;
+let cantPelicula = parseInt(prompt("Cuantas peliculas queres elegir?"))
+let nombrePelicula = "";
+let precioPelicula = 0;
+let sumaTotal= 0;
 
-// for(let i=1; i<=cantPelicula; i++){
-//     nombrePelicula = prompt(`Introduce el nombre de la pelicula #: ${i}`);
-//     precioPelicula = parseInt(prompt(`Introduce el precio de la pelicula #: ${i}`));
-//     alert(`Ingresaste: ${nombrePelicula} $${precioPelicula}`);
-//     sumaTotal += precioPelicula;
-// }
+for(let i=1; i<=cantPelicula; i++){
+    nombrePelicula = prompt(`Introduce el nombre de la pelicula #: ${i}`);
+    precioPelicula = parseInt(prompt(`Introduce el precio de la pelicula #: ${i}`));
+    alert(`Ingresaste: ${nombrePelicula} $${precioPelicula}`);
+    sumaTotal += precioPelicula;
+}
 
-// alert(`El valor de total es de: $${sumaTotal}`);
+alert(`El valor de total es de: $${sumaTotal}`);
 
 // WHILE
 
-let nombrePelicula2= " ";
-let precioPelicula2 = 0;
-let sumaTotal2= 0;
-let limite = parseInt(prompt("De cuanto es tu limite?"))
+// let nombrePelicula2= " ";
+// let precioPelicula2 = 0;
+// let sumaTotal2= 0;
+// let limite = parseInt(prompt("De cuanto es tu limite?"))
 
-while(nombrePelicula2.toUpperCase() != "SALIR" ){
-    nombrePelicula2 = prompt("Elegi una pelicula:");
-    precioPelicula2 = parseInt(prompt("El precio de la pelicula es:"));
-    alert(`Elegiste ${nombrePelicula2} $${precioPelicula2}`);
-    sumaTotal2 += precioPelicula2;
+// while(nombrePelicula2.toUpperCase() != "SALIR" ){
+//     nombrePelicula2 = prompt("Elegi una pelicula:");
+//     precioPelicula2 = parseInt(prompt("El precio de la pelicula es:"));
+//     alert(`Elegiste ${nombrePelicula2} $${precioPelicula2}`);
+//     sumaTotal2 += precioPelicula2;
 
-    alert(`El valor por el momento es de: ${sumaTotal2}`);
+//     alert(`El valor por el momento es de: ${sumaTotal2}`);
 
-    if(sumaTotal2 > limite ){
-        break;
-    }
-}
+//     if(sumaTotal2 > limite ){
+//         break;
+//     }
+// }
 
-alert(`El valor total es de: ${sumaTotal2}`)
+// alert(`El valor total es de: ${sumaTotal2}`)
