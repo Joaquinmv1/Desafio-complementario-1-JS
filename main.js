c4dc569d780906569976e3c5009a0c594c55fdcb
Joaquinvillarreal <villarrealjoaquin09@gmail.com>
2022-06-15 14:04:59 -0300

    while

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-//FOR
-let cantPelicula = parseInt(prompt("Cuantas peliculas queres elegir?"))
-let nombrePelicula = "";
-let precioPelicula = 0;
-let sumaTotal= 0;
+// FOR
+// let cantPelicula = parseInt(prompt("Cuantas peliculas queres elegir?"))
+// let nombrePelicula = "";
+// let precioPelicula = 0;
+// let sumaTotal= 0;
 
-for(let i=1; i<=cantPelicula; i++){
-    nombrePelicula = prompt(`Introduce el nombre de la pelicula n#: ${i}`);
-    precioPelicula = parseInt(prompt(`Introduce el precio de la pelicula n#: $${i}`));
-    alert(`Ingresaste ${nombrePelicula} ${precioPelicula}`);
-    sumaTotal += precioPelicula;
-}
+// for(let i=1; i<=cantPelicula; i++){
+//     nombrePelicula = prompt(`Introduce el nombre de la pelicula #: ${i}`);
+//     precioPelicula = parseInt(prompt(`Introduce el precio de la pelicula #: ${i}`));
+//     alert(`Ingresaste: ${nombrePelicula} $${precioPelicula}`);
+//     sumaTotal += precioPelicula;
+// }
 
-alert(`El valor de total es de: ${sumaTotal}`);
+// alert(`El valor de total es de: $${sumaTotal}`);
 
-//WHILE
+// WHILE
 
 let nombrePelicula2= " ";
 let precioPelicula2 = 0;
@@ -21,12 +21,12 @@ let sumaTotal2= 0;
 let limite = parseInt(prompt("De cuanto es tu limite?"))
 
 while(nombrePelicula2.toUpperCase() != "SALIR" ){
-    nombrePelicula2 = prompt("Elegi una pelicula!");
+    nombrePelicula2 = prompt("Elegi una pelicula:");
     precioPelicula2 = parseInt(prompt("El precio de la pelicula es:"));
     alert(`Elegiste ${nombrePelicula2} $${precioPelicula2}`);
     sumaTotal2 += precioPelicula2;
 
-    console.log(`El valor por el momento es de: ${sumaTotal2}`);
+    alert(`El valor por el momento es de: ${sumaTotal2}`);
 
     if(sumaTotal2 > limite ){
         break;
