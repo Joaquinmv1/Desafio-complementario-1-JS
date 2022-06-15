7c8377d63e1bb628b4c13c71bd4686aa3fd32e8d
Joaquinvillarreal <villarrealjoaquin09@gmail.com>
2022-06-15 13:48:50 -0300

    for

---
+++ main.js
@@ -0,0 +1,36 @@
+//FOR
+let cantPelicula = parseInt(prompt("Cuantas peliculas queres elegir?"))
+let nombrePelicula = "";
+let precioPelicula = 0;
+let sumaTotal= 0;
+
+for(let i=1; i<=cantPelicula; i++){
+    nombrePelicula = prompt(`Introduce el nombre de la pelicula n#: ${i}`);
+    precioPelicula = parseInt(prompt(`Introduce el precio de la pelicula n#: $${i}`));
+    alert(`Ingresaste ${nombrePelicula} ${precioPelicula}`);
+    sumaTotal += precioPelicula;
+}
+
+alert(`El valor de total es de: ${sumaTotal}`);
+
+//WHILE
+
+let nombrePelicula2= " ";
+let precioPelicula2 = 0;
+let sumaTotal2= 0;
+let limite = parseInt(prompt("De cuanto es tu limite?"))
+
+while(nombrePelicula2.toUpperCase() != "SALIR" ){
+    nombrePelicula2 = prompt("Elegi una pelicula!");
+    precioPelicula2 = parseInt(prompt("El precio de la pelicula es:"));
+    alert(`Elegiste ${nombrePelicula2} $${precioPelicula2}`);
+    sumaTotal2 += precioPelicula2;
+
+    console.log(`El valor por el momento es de: ${sumaTotal2}`);
+
+    if(sumaTotal2 > limite ){
+        break;
+    }
+}
+
+alert(`El valor total es de: ${sumaTotal2}`)
